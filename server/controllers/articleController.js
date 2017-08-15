a98ae925a06fe70f7add463dbf617650d83e67e3
jumadiakhmad <jumadiakhmad2@gmail.com>
2017-08-15 14:01:54 +0700

    End Point 2

---
+++ server/controllers/articleController.js
@@ -0,0 +1,87 @@
+const Articles = require('../models/articleModel');
+const Users = require('../models/userModel');
+
+var createArticle = (req,res) => {
+  var newArticle = new Articles ({
+    title: req.body.title,
+    content: req.body.content,
+    category: req.body.category,
+    author: req.body.author
+  })
+
+  newArticle.save( (err, response) => {
+    if(err) {
+      res.send(err)
+    } else {
+      Articles.findById(response._id
+      .populate('author')
+      .then( (err,result) => {
+        if(err) res.send(err)
+        console.log('Success Tambah Data', result);
+        res.send(result)
+      })
+    }
+  })
+}
+
+var getAllArticles = (req,res) => {
+  Articles.find((err,articles) => {
+    if(err) res.send(err)
+    console.log(articles);
+    res.send(articles)
+  })
+}
+
+var getArticleByAuthor = (req,res) => {
+  Articles.find()
+  .populate('author')
+  .then( (err,response) => {
+    var push
+    articlePush = []
+    response.forEach( article => {
+      if(article._id == req.params.id) {
+        articlePush.push(article)
+      }
+    })
+    res.send(articlePush)
+  })
+}
+
+var updateArticle = (req,res) => {
+  Articles.update({
+    _id : req.params.id
+  }, {
+    $set: {
+      title: req.body.title,
+      content: req.body.content,
+      category: req.body.category,
+      author: req.body.author
+    }
+  })
+  .then((err,result) => {
+    if(err) {
+      res.send(err)
+    } else {
+      Articles.findById(result._id)
+      .populate('author')
+      .then( (err,article) => {
+        if(err) res.send(err)
+        res.send(article)
+      })
+    }
+  })
+}
+
+var deleteArticle = (req,res) => {
+  let id = req.params.id;
+  Articles.findOneAndRemove(id, (err,result) => {
+    if(err) res.send(err)
+    console.log('delete article success', result);
+    res.send(result)
+  })
+}
+
+module.exports = {
+  createArticle, getAllArticles, getArticleByAuthor,
+  updateArticle, deleteArticle
+}
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,56 @@
+require('dotenv').config()
 const Users = require('../models/userModel');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken')
+const secret = process.env.SECRET_KEY;
 
 var signIn = (req,res) => {
-  Users.findOne()
+  var password = req.body.password
+  Users.findOne({username: req.body.password}, (err,response) => {
+    if(err) {
+        res.send(err)
+    } else {
+        if(bcrypt.compareSync(password, response.password) {
+          let token = jwt.sign({
+            id: response._id,
+            username: resonse.username,
+            email: response.email
+          }, secret, {expires: '1d'})
+          res.json({
+            username: response._id,
+            email: response.email,
+            token
+          })
+        }
+    } else {
+      res.send(err)
+    }
+  })
+}
+
+var signUp = (req,res) => {
+  var password = req.body.password
+  var newUser = new Users({
+    name: req.body.name,
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password
+  })
+
+  newUser.save( (err,user) => {
+    if(err) res.send(err)
+    console.log(user);
+    res.send(user)
+  })
+}
+
+var getAllUsers = (req,res) => {
+  Users.find({}, (err,users) => {
+    if(err) res.send(err)
+    res.send(users)
+  })
+}
+
+module.exports = {
+  signIn, signUp, getAllUsers
 }
